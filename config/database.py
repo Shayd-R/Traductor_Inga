f726dbc31cc8185017211123603a5aba842e5d1b
krodzito <shaydruano@gmail.com>
2022-11-15 11:32:15 -0500

    conexion con la BD y consultas

--- config/database.py
+++ config/database.py
@@ -2,11 +2,11 @@ import mysql.connector
 from config import settings
 
 db = mysql.connector.connect(
-    host=settings.MYSQL_HOSTNAME,
-    user=settings.MYSQL_USERNAME,
-    password=settings.MYSQL_PASSWORD,
-    port=settings.MYSQL_PORT,
-    database=settings.MYSQL_DATABASE,
+    host='localhost',
+    user='root',
+    password='',
+    port=3306,
+    database='traductor',
     buffered=True
 )
 db.autocommit = True
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 from flask import Flask, jsonify,render_template,request,redirect,url_for,session,flash,json,make_response
 from models import voz_traductor, traduccion_texto
+from config.database import db
 
 app = Flask(__name__)
 app.secret_key = "##91!IasdyAjadfbdfan"
@@ -8,7 +9,7 @@ app.secret_key = "##91!IasdyAjadfbdfan"
 @app.route("/", methods=["GET", "POST"])
 def inicio():
 
-    return render_template("/traductor/asd.html")
+    return render_template("/traductor/traductor.html")
 
 @app.route("/texto", methods=["GET", "POST"])
 def texto():
@@ -17,13 +18,17 @@ def texto():
         texto=texto_entrada.split()
         traducido=[]
         for palabra in texto:
-            if palabra=="hola":
-                a="hello"
-                traducido.append(a) 
-            else:
+            print(palabra)
+            cursor = db.cursor(dictionary=True)
+            cursor.execute("SELECT * FROM palabras_espanol WHERE palabra_espanol='"+palabra+"' OR palabra_espanol LIKE '%"+palabra+"%' ")
+            a=cursor.fetchone()
+            if a==None:
                 a=palabra
-                traducido.append(a)  
-        texto_salida=traduccion_texto.concatenar_palabras(traducido, " ")
+            else:
+                a=a['traduccion']   
+            traducido.append(a)
+            db.commit()
+        texto_salida=traduccion_texto.concatenar_palabras(traducido, "  ")
         return render_template("/traductor/traductor.html", texto_salida=texto_salida, texto_entrada=texto_entrada)
     return render_template("/traductor/traductor.html")
 
@@ -34,13 +39,16 @@ def audio():
     texto=texto_entrada.split()
     traducido=[]
     for palabra in texto:
-        if palabra=="hola":
-            a="hello"
-            traducido.append(a) 
-        else:
+        cursor = db.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM palabras_espanol WHERE palabra_espanol='"+palabra+"' OR palabra_espanol LIKE '%"+palabra+"%' ")
+        a=cursor.fetchone()
+        if a==None:
             a=palabra
-            traducido.append(a)  
-    texto_salida=traduccion_texto.concatenar_palabras(traducido, " ")
+        else:
+            a=a['traduccion']   
+        traducido.append(a)
+        db.commit()
+        texto_salida=traduccion_texto.concatenar_palabras(traducido, "  ")
     voz_traductor.voz_texto(texto_salida)
     return render_template("/traductor/traductor.html", texto_entrada=texto_entrada, texto_salida=texto_salida)
     
